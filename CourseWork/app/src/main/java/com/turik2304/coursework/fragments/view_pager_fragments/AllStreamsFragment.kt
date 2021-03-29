c8cbc04224e120dca4dfbeea004c3fc70180bda7
turik2304 <asibag98@gmail.com>
2021-03-29 17:52:25 +0300

    expandable streams in AllStreamsFragment done

--- CourseWork/app/src/main/java/com/turik2304/coursework/fragments/view_pager_fragments/AllStreamsFragment.kt
+++ CourseWork/app/src/main/java/com/turik2304/coursework/fragments/view_pager_fragments/AllStreamsFragment.kt
@@ -6,21 +6,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
-import android.widget.LinearLayout
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import com.turik2304.coursework.ChatActivity
 import com.turik2304.coursework.R
+import com.turik2304.coursework.fragments.view_pager_fragments.SubscribedFragment.Companion.animateExpandImageViewFrom0to180
+import com.turik2304.coursework.fragments.view_pager_fragments.SubscribedFragment.Companion.animateExpandImageViewFrom180to0
+import com.turik2304.coursework.fragments.view_pager_fragments.SubscribedFragment.Companion.getTopicsUIListByStreamUid
 import com.turik2304.coursework.network.FakeServerApi
 import com.turik2304.coursework.network.ServerApi
 import com.turik2304.coursework.recycler_view_base.AsyncAdapter
 import com.turik2304.coursework.recycler_view_base.DiffCallback
 import com.turik2304.coursework.recycler_view_base.ViewTyped
 import com.turik2304.coursework.recycler_view_base.holder_factories.MainHolderFactory
+import com.turik2304.coursework.recycler_view_base.items.StreamAndTopicSeparatorUI
 import com.turik2304.coursework.recycler_view_base.items.StreamUI
+import com.turik2304.coursework.recycler_view_base.items.TopicUI
 
 class AllStreamsFragment : Fragment() {
 
+    private lateinit var innerViewTypedList: List<ViewTyped>
+    private lateinit var listOfStreams: List<ViewTyped>
+    private lateinit var asyncAdapter: AsyncAdapter<ViewTyped>
     private val fakeServer: ServerApi = FakeServerApi()
 
     override fun onCreateView(
@@ -36,28 +43,80 @@ class AllStreamsFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         val recyclerViewAllStreams = view.findViewById<RecyclerView>(R.id.recycleViewAllStreams)
-        val clickListener = { clickedView: View ->
-            when (clickedView) {
-                is ImageView -> {
-                    clickedView.setImageResource(R.drawable.ic_arrow_up_24)
+        val listOfExpandedStreams = mutableListOf<String>()
+        val clickListener = clickListener@{ clickedView: View ->
+            val positionOfClickedView =
+                recyclerViewAllStreams.getChildAdapterPosition(clickedView)
+            val clickedItem = innerViewTypedList[positionOfClickedView]
+            if (clickedItem.viewType == R.layout.item_stream) {
+                val expandImageView = clickedView.findViewById<ImageView>(R.id.imExpandStream)
+                val uidOfStreamUI = clickedItem.uid
+                if (uidOfStreamUI in listOfExpandedStreams) {
+                    listOfExpandedStreams.remove(uidOfStreamUI)
+                    (clickedItem as StreamUI).isExpanded = false
+                    animateExpandImageViewFrom180to0(context, expandImageView)
+                    clickedView.setBackgroundColor(
+                        resources.getColor(
+                            R.color.gray_primary_background,
+                            context?.theme
+                        )
+                    )
+                } else {
+                    listOfExpandedStreams.add(uidOfStreamUI)
+                    (clickedItem as StreamUI).isExpanded = true
+                    animateExpandImageViewFrom0to180(context, expandImageView)
+                    clickedView.setBackgroundColor(
+                        resources.getColor(
+                            R.color.gray_secondary_background,
+                            context?.theme
+                        )
+                    )
                 }
-                is LinearLayout -> {
-                    val intent = Intent(context, ChatActivity::class.java)
-                    startActivity(intent)
+                innerViewTypedList = listOfStreams.flatMap { stream ->
+                    listOf(stream) + if (stream.uid in listOfExpandedStreams) {
+                        getTopicsUIListByStreamUid(stream.uid, fakeServer)
+                    } else emptyList()
                 }
+                asyncAdapter.items.submitList(innerViewTypedList)
+            } else {
+                val uidOfClickedTopicUI = clickedItem.uid
+                //some logic to start chat by topic ID
+                var indexOfTopicUI = 0
+                var indexOfStreamUI = 0
+                innerViewTypedList.forEachIndexed { index, viewTyped ->
+                    if (viewTyped.uid == uidOfClickedTopicUI) indexOfTopicUI = index
+                }
+                val nameOfTopic = (innerViewTypedList[indexOfTopicUI] as TopicUI).name
+                while (innerViewTypedList[indexOfTopicUI].viewType != R.layout.item_stream) {
+                    indexOfStreamUI = indexOfTopicUI
+                    indexOfTopicUI--
+                }
+                val nameOfStreamUI = (innerViewTypedList[indexOfStreamUI - 1] as StreamUI).name
+                val intent = Intent(context, ChatActivity::class.java)
+                intent.putExtra(SubscribedFragment.EXTRA_NAME_OF_TOPIC, nameOfTopic)
+                intent.putExtra(SubscribedFragment.EXTRA_NAME_OF_STREAM, nameOfStreamUI)
+                startActivity(intent)
             }
+            return@clickListener
         }
         val holderFactory = MainHolderFactory(clickListener)
         val diffCallBack = DiffCallback<ViewTyped>()
 
-        val asyncAdapter = AsyncAdapter(holderFactory, diffCallBack)
+        asyncAdapter = AsyncAdapter(holderFactory, diffCallBack)
         recyclerViewAllStreams.adapter = asyncAdapter
-        asyncAdapter.items.submitList(getStreamsAndTopicsItemsFromFakeServer())
+        innerViewTypedList = getAllStreamsFromFakeServer()
+        listOfStreams = innerViewTypedList
+        asyncAdapter.items.submitList(listOfStreams)
     }
 
-    private fun getStreamsAndTopicsItemsFromFakeServer(): List<ViewTyped> {
-        return fakeServer.allStreams.map { (name, uid) ->
-            StreamUI(name, uid)
+    private fun getAllStreamsFromFakeServer(): List<ViewTyped> {
+        return fakeServer.allStreams.flatMap { (streamName, uid) ->
+            listOf(
+                StreamUI(
+                    streamName,
+                    uid
+                )
+            ) + listOf(StreamAndTopicSeparatorUI(uid = "STREAM_SEPARATOR_$uid"))
         }
 
     }
--- CourseWork/app/src/main/java/com/turik2304/coursework/recycler_view_base/holders/StreamHolder.kt
+++ CourseWork/app/src/main/java/com/turik2304/coursework/recycler_view_base/holders/StreamHolder.kt
@@ -1,6 +1,7 @@
 package com.turik2304.coursework.recycler_view_base.holders
 
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 import com.turik2304.coursework.R
 import com.turik2304.coursework.recycler_view_base.BaseViewHolder
@@ -12,6 +13,7 @@ class StreamHolder(
 ) : BaseViewHolder<StreamUI>(view) {
 
     private val nameOfStreamHolder = view.findViewById<TextView>(R.id.tvNameOfStream)
+    private val expandImageView = view.findViewById<ImageView>(R.id.imExpandStream)
 
     init {
         view.setOnClickListener(getTopicsClick)
@@ -19,5 +21,22 @@ class StreamHolder(
 
     override fun bind(item: StreamUI) {
         nameOfStreamHolder.text = item.name
+        if (item.isExpanded) {
+            expandImageView.setImageResource(R.drawable.ic_arrow_up_24)
+            itemView.setBackgroundColor(
+                itemView.resources.getColor(
+                    R.color.gray_secondary_background,
+                    itemView.context.theme
+                )
+            )
+        } else {
+            expandImageView.setImageResource(R.drawable.ic_arrow_down_24)
+            itemView.setBackgroundColor(
+                itemView.resources.getColor(
+                    R.color.gray_primary_background,
+                    itemView.context.theme
+                )
+            )
+        }
     }
 }
